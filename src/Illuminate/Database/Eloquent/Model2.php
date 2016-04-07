6eac872cee7a6bfa5cff8cb2c68276333316c247
Juan R. Pozo <jrpozo@g-a-t.es>
2016-04-07 21:37:29 +0200

    Initial commit

---
+++ src/Illuminate/Database/Eloquent/Model2.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Illuminate\Database\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Model2 extends Model
+{
+    /**
+     * Define a Rails-like has-many-through relationship.
+     *
+     * @param  string  $related
+     * @param  string  $through
+     * @param  string|null  $firstKey
+     * @param  string|null  $secondKey
+     * @param  string|null  $localKey
+     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+     */
+    public function hasManyThrough2($related, $through, $firstKey = null, $secondKey = null, $localKey = null)
+    {
+        $through = new $through;
+
+        $firstKey = $firstKey ?: $this->getForeignKey();
+
+        $secondKey = $secondKey ?: $through->getForeignKey();
+
+        $localKey = $localKey ?: $this->getKeyName();
+
+        return new HasManyThrough((new $related)->newQuery(), $this, $through, $firstKey, $secondKey, $localKey);
+    }
+}
